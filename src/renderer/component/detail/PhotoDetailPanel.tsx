1d9b0497c073b66fbc03b5aa3b219bad661d93f5
vayacico <vayacico@gmail.com>
2024-06-05 22:12:16 +0900

    ワールド名等のコピー機能を追加

--- src/renderer/component/detail/PhotoDetailPanel.tsx
+++ src/renderer/component/detail/PhotoDetailPanel.tsx
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 import {
+  BsCheck2,
+  BsCopy,
   BsFillCalendarEventFill,
   BsFillCameraFill,
   BsFillFileEarmarkTextFill,
   BsFillPersonFill,
   BsGlobe,
+  BsLink45Deg,
 } from 'react-icons/bs';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Tooltip } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
   worldName: string;
@@ -31,9 +36,28 @@ const LinkItem = styled.h2`
   margin-top: 0;
   margin-left: 16px;
   margin-bottom: 4px;
+  margin-right: 16px;
   color: #38393d;
   font-weight: 500;
   max-width: 300px;
+
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`;
+
+const PathItem = styled.h2`
+  font-size: 18px;
+  font-family: '游ゴシック', system-ui;
+  margin-top: 0;
+  margin-left: 16px;
+  margin-bottom: 4px;
+  margin-right: 16px;
+  color: #38393d;
+  font-weight: 500;
+  max-width: 300px;
+  word-break: break-all;
 
   &:hover {
     cursor: pointer;
@@ -57,6 +81,7 @@ const IconTextWrapper = styled.div`
   display: flex;
   margin-left: 20px;
   margin-bottom: 20px;
+  margin-right: 20px;
 `;
 
 const WorldIcon = styled(BsGlobe)`
@@ -89,6 +114,29 @@ const PhotoFileIcon = styled(BsFillFileEarmarkTextFill)`
   font-size: 20px;
   margin-top: 4px;
 `;
+const CopyIconWrapper = styled.span`
+  width: 24px;
+  height: 32px;
+  font-size: 16px;
+`;
+const CopyIcon = styled(BsCopy)`
+  color: #6e6e6e;
+
+  &:hover {
+    color: #38393d;
+  }
+`;
+const UrlCopyIcon = styled(BsLink45Deg)`
+  color: #888787;
+  font-size: 1.4em;
+
+  &:hover {
+    color: #38393d;
+  }
+`;
+const CustomCheckIcon = styled(BsCheck2)`
+  color: #188036;
+`;
 
 const PhotoDetailPanel: React.FC<Props> = (props) => {
   const ids = props.instanceId && props.instanceId.split(':');
@@ -97,6 +145,41 @@ const PhotoDetailPanel: React.FC<Props> = (props) => {
   }`;
   const splitedPath = props.originalFilePath.split('\\');
   const fileName = splitedPath[splitedPath.length - 1];
+
+  const [worldNameCopied, setWorldNameCopied] = useState(false);
+  const [pathCopied, setPathCopied] = useState(false);
+  const [urlCopied, setUrlCopied] = useState(false);
+  const [t] = useTranslation();
+
+  useEffect(() => {
+    setWorldNameCopied(false);
+    setPathCopied(false);
+    setUrlCopied(false);
+  }, [props.originalFilePath, props.worldName]);
+
+  const copyWorldNameToClipboard = () => {
+    navigator.clipboard.writeText(props.worldName);
+    setWorldNameCopied(true);
+    setTimeout(() => {
+      setWorldNameCopied(false);
+    }, 5000);
+  };
+
+  const copyPathToClipboard = () => {
+    navigator.clipboard.writeText(props.originalFilePath);
+    setPathCopied(true);
+    setTimeout(() => {
+      setPathCopied(false);
+    }, 5000);
+  };
+
+  const copyUrlToClipboard = () => {
+    navigator.clipboard.writeText(url);
+    setUrlCopied(true);
+    setTimeout(() => {
+      setUrlCopied(false);
+    }, 5000);
+  };
 
   return (
     <DetailPanel>
@@ -109,6 +192,24 @@ const PhotoDetailPanel: React.FC<Props> = (props) => {
         >
           {props.worldName}
         </LinkItem>
+        <Tooltip
+          label={worldNameCopied ? t('panel.copied') : t('panel.copyWorldName')}
+          placement="top"
+          closeOnClick={false}
+        >
+          <CopyIconWrapper onClick={() => copyWorldNameToClipboard()}>
+            {worldNameCopied ? <CustomCheckIcon /> : <CopyIcon />}
+          </CopyIconWrapper>
+        </Tooltip>
+        <Tooltip
+          label={urlCopied ? t('panel.copied') : t('panel.copyWorldUrl')}
+          placement="top"
+          closeOnClick={false}
+        >
+          <CopyIconWrapper onClick={() => copyUrlToClipboard()}>
+            {urlCopied ? <CustomCheckIcon /> : <UrlCopyIcon />}
+          </CopyIconWrapper>
+        </Tooltip>
       </IconTextWrapper>
       <IconTextWrapper>
         <CalendarIcon />
@@ -118,7 +219,7 @@ const PhotoDetailPanel: React.FC<Props> = (props) => {
       </IconTextWrapper>
       <IconTextWrapper>
         <PhotoFileIcon />
-        <LinkItem
+        <PathItem
           onClick={() =>
             window.service.application.openFileInDefaultApplication(
               props.originalFilePath
@@ -130,7 +231,16 @@ const PhotoDetailPanel: React.FC<Props> = (props) => {
           onMouseLeave={() => props.setStatus && props.setStatus('')}
         >
           {fileName}
-        </LinkItem>
+        </PathItem>
+        <Tooltip
+          label={pathCopied ? t('panel.copied') : t('panel.copyFilePath')}
+          placement="top"
+          closeOnClick={false}
+        >
+          <CopyIconWrapper onClick={() => copyPathToClipboard()}>
+            {pathCopied ? <CustomCheckIcon /> : <CopyIcon />}
+          </CopyIconWrapper>
+        </Tooltip>
       </IconTextWrapper>
       <IconTextWrapper>
         <CameraIcon />
