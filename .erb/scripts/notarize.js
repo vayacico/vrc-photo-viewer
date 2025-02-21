7a1360c2069707d0707c97c94c1ce80e7b614a18
vayacico <vayacico@gmail.com>
2025-02-22 00:07:35 +0900

    Electronのバージョンアップ

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -1,4 +1,4 @@
-const { notarize } = require('electron-notarize');
+const { notarize } = require('@electron/notarize');
 const { build } = require('../../package.json');
 
 exports.default = async function notarizeMacos(context) {
@@ -7,22 +7,32 @@ exports.default = async function notarizeMacos(context) {
     return;
   }
 
-  if (process.env.CI !== "true") {
+  if (process.env.CI !== 'true') {
     console.warn('Skipping notarizing step. Packaging is not running in CI');
     return;
   }
 
-  if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env)) {
-    console.warn('Skipping notarizing step. APPLE_ID and APPLE_ID_PASS env variables must be set');
+  if (
+    !(
+      'APPLE_ID' in process.env &&
+      'APPLE_ID_PASS' in process.env &&
+      'APPLE_TEAM_ID' in process.env
+    )
+  ) {
+    console.warn(
+      'Skipping notarizing step. APPLE_ID, APPLE_ID_PASS, and APPLE_TEAM_ID env variables must be set',
+    );
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
   await notarize({
+    tool: 'notarytool',
     appBundleId: build.appId,
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_ID_PASS,
+    teamId: process.env.APPLE_TEAM_ID,
   });
 };
